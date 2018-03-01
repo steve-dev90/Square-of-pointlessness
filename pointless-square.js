2dcf545d4fe1897f83f1d33420b5574cf5030e1c
Steve Torrens <stevetorrens@Steves-MacBook-Air.local>
2018-03-01 13:04:08 +1300

    Refactored animation box animation

--- pointless-square.js
+++ pointless-square.js
@@ -1,7 +1,7 @@
 //Request animation blue print from http://www.javascriptkit.com/javatutors/requestanimationframe.shtml
 
 var starttime
-var squareSpeed = 9000
+var squareDuration = 9000
 var speedIncrement = 250
 
 document.addEventListener('DOMContentLoaded', start)
@@ -13,7 +13,7 @@ function start () {
 
     requestAnimationFrame(function(timestamp){
         starttime = timestamp || new Date().getTime() //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date
-        moveit(timestamp, box) 
+        moveit(timestamp, box, 500, 2000) 
     })
 
 }
@@ -30,18 +30,43 @@ function increaseSquareSpeed() {
 }
 
 
-function moveit(timestamp, el) {
+function moveit(timestamp, element, distance, duration) {
     //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date:
     var timestamp = timestamp || new Date().getTime()
     var runtime = timestamp - starttime
-    var dist = document.getElementById('canvas').offsetWidth
-    moveBox(el, runtime, dist)
+    moveElement(runtime, element, distance, duration)
+    animateElement(timestamp, runtime, element, distance, duration)
+}
 
-    if (runtime < squareSpeed){ // if duration not met yet
+function moveElement(runtime, element, distance, duration) {
+    var progress = runtime / duration
+    progress = Math.min(progress, 1)
+    element.style.left = (distance * progress).toFixed(2) + 'px' 
+}
+
+function animateElement(timestamp, runtime, element, distance, duration) {
+    if (runtime < duration){ // if duration not met yet
         requestAnimationFrame(function(timestamp){ // call requestAnimationFrame again with parameters
-            moveit(timestamp, el)
+            moveit(timestamp, element, distance, duration)
         }) 
     } 
+    // else {
+
+    // }
+}
+
+// function transit(timestamp, element, distance, duration, moveElement) {
+//     //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date:
+//     var timestamp = timestamp || new Date().getTime()
+//     var runtime = timestamp - starttime
+//     moveElement(runtime, element, distance, duration)
+//     animateElement(timestamp, runtime, element, distance, duration)
+// })
+
+
+
+
+
     // else {
     // //     el.style.left = '0px' 
     //     starttime = timestamp || new Date().getTime() 
@@ -49,10 +74,5 @@ function moveit(timestamp, el) {
     //         moveit(timestamp, el, dist)
     //     })
     // } 
-}
+
  
-function moveBox(el, runtime, dist) {
-    var progress = runtime / squareSpeed
-    progress = Math.min(progress, 1)
-    el.style.left = (dist * progress).toFixed(2) + 'px' 
-}
